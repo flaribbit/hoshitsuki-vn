76d8d9ee6a4953f464d9371fd9a0c1060abbb306
flaribbit <1149761294@qq.com>
2021-09-22 22:57:31 +0800

    画面雏形

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,13 @@
 <script setup>
-import HelloWorld from "./components/HelloWorld.vue";
-import("./assets/demogame.story").then((data) => {
-  console.log(data);
-});
+import Game from "./components/Game.vue";
 </script>
 
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Game />
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+body {
+  margin: 0;
 }
 </style>
---
+++ src/components/Game.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="game">
+    <h1>game</h1>
+    <div class="dialog-name">{{ name }}</div>
+    <div class="dialog-box">{{ text }}</div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+const name = ref("");
+const text = ref("");
+import("../assets/demogame.story").then(({ default: data }) => {
+  console.log(data);
+  name.value = "name";
+  text.value = data[0][1];
+});
+</script>
+
+<style>
+.game {
+  position: absolute;
+  width: 1280px;
+  height: 720px;
+  border: 1px solid;
+  font-size: 24px;
+}
+.dialog-name {
+  position: absolute;
+  left: 100px;
+  bottom: 180px;
+  min-width: 100px;
+  padding: 20px;
+  border: 4px solid;
+  border-radius: 10px;
+}
+.dialog-box {
+  position: absolute;
+  left: 100px;
+  bottom: 20px;
+  top: 540px;
+  right: 100px;
+  padding: 20px;
+  border: 4px solid;
+  border-radius: 10px;
+}
+</style>
