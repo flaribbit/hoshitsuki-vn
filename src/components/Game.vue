b7743d02309750f5a1edec2246cd5964ef7e7979
flaribbit <1149761294@qq.com>
2021-09-23 00:01:52 +0800

    背景图

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="game">
     <h1>game</h1>
+    <div class="background"></div>
     <div class="dialog-name">{{ name }}</div>
     <div class="dialog-box">{{ text }}</div>
   </div>
@@ -24,14 +25,27 @@ import("../assets/demogame.story").then(({ default: data }) => {
   height: 720px;
   border: 1px solid;
   font-size: 24px;
+  --c-line: #f6bd60;
+  --c-background: #f7ede2;
+}
+.background {
+  position: absolute;
+  background: url(../assets/backgrounds/道02.png) no-repeat center;
+  background-size: cover;
+  image-rendering: pixelated;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 .dialog-name {
   position: absolute;
   left: 100px;
   bottom: 180px;
   min-width: 100px;
-  padding: 20px;
-  border: 4px solid;
+  padding: 10px 20px;
+  border: 4px solid var(--c-line);
+  background: var(--c-background);
   border-radius: 10px;
 }
 .dialog-box {
@@ -41,7 +55,8 @@ import("../assets/demogame.story").then(({ default: data }) => {
   top: 540px;
   right: 100px;
   padding: 20px;
-  border: 4px solid;
+  border: 4px solid var(--c-line);
+  background: var(--c-background);
   border-radius: 10px;
 }
 </style>
