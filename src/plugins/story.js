0d8e6266c4dfddaff34925bc4544ffcdb6842ae7
flaribbit <1149761294@qq.com>
2021-09-24 13:15:41 +0800

    编译goto label

--- src/plugins/story.js
+++ src/plugins/story.js
@@ -5,13 +5,24 @@ const regexLine = /^(\w+) (.+)/;
 function compileFileToJS(src) {
   var lines = src.split(/\r?\n/);
   var cmds = [];
+  var labels = {};
+  var index = 0;
   for (var line of lines) {
     if (line == "") continue;
     var r = regexLine.exec(line);
-    if (r && commands.indexOf(r[1]) != -1) {
+    if (r && r[1] == "label") {
+      labels[r[2]] = index;
+    } else if (r && commands.indexOf(r[1]) != -1) {
       cmds.push([r[1], r[2]]);
+      index++;
     } else {
       cmds.push(["text", line]); //省略指令时默认text
+      index++;
+    }
+  }
+  for (var cmd of cmds) {
+    if (cmd[0] == "goto") {
+      cmd[1] = labels[cmd[1]];
     }
   }
   return "export default " + JSON.stringify(cmds);
