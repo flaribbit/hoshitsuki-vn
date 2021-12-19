dde5b8a5bfb878c2537a4df00064965738299e54
flaribbit <1149761294@qq.com>
2021-12-19 13:30:00 +0800

    使用switch重构剧本编译

--- src/plugins/story.ts
+++ src/plugins/story.ts
@@ -9,6 +9,7 @@ function compileFileToJS(src: string) {
   const lines: string[] = src.split(/\r?\n/);
   const cmds = [];
   const labels: { [label: string]: number } = {};
+  let s: string;
   let index = 0;
   for (let line of lines) {
     if (line == "") continue; //skip empty lines
@@ -17,42 +18,52 @@ function compileFileToJS(src: string) {
       //fallback to text if there is no command
       r = regexLine.exec("text " + line);
     }
-    if (r[1] == "label") { //save labels
-      labels[r[2]] = index;
-      continue;
-    }
-    if (r[1] == "goto") { //preprocess goto command
-      const s = r[2];
-      r = regexGoto.exec(s);
-      if (r) {
-        cmds.push(["goto", r[1], r[2], r[3], r[4], parseFloat(r[5])]);
-      } else {
-        cmds.push(["goto", s]);
-      }
-    } else if (r[1] == "text") { //preprocess text command
-      const s = r[2];
-      r = regexText.exec(r[2]);
-      if (r) {
-        cmds.push(["text", { n: r[1], t: r[2] }]); //n = name, t = text
-      } else {
-        cmds.push(["text", { n: null, t: s }]);
-      }
-    } else if (r[1] == "set" || r[1] == "add") { //preprocess set/add command
-      const s = r[1];
-      r = regexSet.exec(r[2]);
-      cmds.push([s, r[1], parseFloat(r[2])]);
-    } else if (r[1] == "actor") { //preprocess actor command
-      const s = r[2].split(" ");
-      cmds.push(["actor", s]);
-    } else if (r[1] == "choice") { //preprocess choice command
-      const s = r[2].split(" ");
-      let choices = [];
-      for (let i = 0; i < s.length; i += 2) {
-        choices.push({ text: s[i], goto: s[i + 1] });
-      }
-      cmds.push([r[1], choices]);
-    } else {
-      cmds.push([r[1], r[2]]);
+    switch (r[1]) {
+      case "label": //save labels
+        labels[r[2]] = index;
+        continue;
+      case "goto": //preprocess goto command
+        s = r[2];
+        r = regexGoto.exec(s);
+        if (r) {
+          cmds.push(["goto", r[1], r[2], r[3], r[4], parseFloat(r[5])]);
+        } else {
+          cmds.push(["goto", s]);
+        }
+        break;
+      case "text": //preprocess text command
+        s = r[2];
+        r = regexText.exec(r[2]);
+        if (r) {
+          cmds.push(["text", { n: r[1], t: r[2] }]); //n = name, t = text
+        } else {
+          cmds.push(["text", { n: null, t: s }]);
+        }
+        break;
+      case "set": //preprocess set command
+      case "add": //preprocess add command
+        s = r[1];
+        r = regexSet.exec(r[2]);
+        cmds.push([s, r[1], parseFloat(r[2])]);
+        break;
+      case "actor": //preprocess actor command
+        cmds.push(["actor", r[2].split(" ")]);
+        break;
+      case "choice":
+        const t = r[2].split(" ");
+        let choices = [];
+        for (let i = 0; i < t.length; i += 2) {
+          choices.push({ text: t[i], goto: t[i + 1] });
+        }
+        cmds.push([r[1], choices]);
+        break;
+      case "scene":
+      case "bg":
+      case "bgm":
+        cmds.push([r[1], r[2]]);
+        break;
+      default:
+        console.log("Unknown command: " + r[1]);
     }
     index++;
   }
