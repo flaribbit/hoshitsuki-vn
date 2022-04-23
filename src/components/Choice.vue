02428f96ff3e2450dc888b0b6ab6dfa036418ab2
flaribbit <1149761294@qq.com>
2022-04-23 21:51:05 +0800

    优化选择项写法

--- src/components/Choice.vue
+++ src/components/Choice.vue
@@ -1,19 +1,16 @@
 <template>
   <div class="choice">
-    <div
-      class="item"
-      v-for="item in props.items"
-      @click.stop="emit('goto', item.goto)"
-    >{{ item.text }}</div>
+    <div class="item" v-for="item in items" @click.stop="emit('goto', item.goto)">{{ item.text }}</div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-const emit = defineEmits(["goto"]);
-const props = defineProps({
-  items: { type: Object, default: [] },
-});
+const emit = defineEmits<{
+  (event: 'goto', goto: number): void
+}>();
+const { items = [] } = defineProps<{
+  items: { goto: number, text: string }[]
+}>();
 </script>
 
 <style>
@@ -23,6 +20,7 @@ const props = defineProps({
   right: 320px;
   top: 160px;
 }
+
 .choice .item {
   border: 4px solid var(--c-line);
   background: var(--c-background);
